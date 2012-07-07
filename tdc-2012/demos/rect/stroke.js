8c01868608e0d6e2d1397c91eef8f0e671686430
zenorocha <zno.rocha@gmail.com>
2012-07-07 20:06:01 -0300

    tdc slides added

---
+++ tdc-2012/demos/rect/stroke.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+
+	var canvas = document.getElementById('stroke');
+	var contexto = canvas.getContext('2d');
+
+	contexto.strokeStyle = "#fff";
+	contexto.strokeRect(25, 25, 100, 100);
+	
+};
